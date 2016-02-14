a956e546037a8826198876c538e7a040cdb578cd
joamag <joamag@gmail.com>
2016-02-14 12:12:48 +0000

    new guppy printing

--- Dockerfile
+++ Dockerfile
@@ -8,12 +8,12 @@ ENV SERVER netius
 ENV SERVER_ENCODING gzip
 ENV HOST 0.0.0.0
 ENV PORT 8080
-ENV MONGOHQ_URL mongodb://localhost:27017
 ENV PYTHONPATH /src
 
 ADD requirements.txt /
+ADD extra.txt /
 ADD src /src
 
-RUN pip install -r /requirements.txt && pip install --upgrade netius
+RUN pip install -r /requirements.txt && pip install -r /extra.txt && pip install --upgrade netius
 
 CMD ["/usr/bin/python", "/src/hello_appier/main.py"]
--- src/hello_appier/scheduler.py
+++ src/hello_appier/scheduler.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import guppy
+
 import appier
 
 class Scheduler(appier.Scheduler):
@@ -9,12 +11,20 @@ class Scheduler(appier.Scheduler):
         appier.Scheduler.__init__(self, owner, timeout = timeout, *args, **kwargs)
         self.requests = appier.conf("HELLO_REQUESTS", requests, cast = int)
         self.asset_url = appier.conf("HELLO_ASSET", "https://httpbin.org/image")
+        self.guppy = appier.conf("HELLO_GUPPY", False, cast = bool)
+        self.heap = None
         self.bytes = 0
 
     def tick(self):
         appier.Scheduler.tick(self)
+        if self.guppy and not self.heap:
+            heap = guppy.hpy()
+            heap.setrelheap()
         self.logger.info("Running remote retrieval process ...")
         for _index in range(self.requests):
             result = appier.get(self.asset_url)
             self.bytes += len(result)
         self.logger.info("Current byte count is %d bytes" % self.bytes)
+        if self.guppy:
+            state = heap.heap()
+            print(state)
