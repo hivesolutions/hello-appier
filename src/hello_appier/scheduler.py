69e6476af850022f656e364ce1f81718f605544a
joamag <joamag@gmail.com>
2016-02-14 16:19:50 +0000

    new psutil support

--- src/hello_appier/scheduler.py
+++ src/hello_appier/scheduler.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import guppy
+import os
 import threading
 
 import appier
+
+try: import guppy
+except: guppy = None
+
+try: import psutil
+except: psutil = None
 
 class Scheduler(appier.Scheduler):
 
@@ -18,16 +24,30 @@ class Scheduler(appier.Scheduler):
 
     def tick(self):
         appier.Scheduler.tick(self)
-        if self.leak and not self.heap:
-            self.heap = guppy.hpy()
-            self.heap.setrelheap()
+        self._init_leak()
         self.logger.info("Running remote retrieval process ...")
         for _index in range(self.requests):
             result = appier.get(self.asset_url)
             self.bytes += len(result)
             del result
         self.logger.info("Current byte count is %d bytes" % self.bytes)
-        if self.leak:
-            state = self.heap.heap()
-            print("%d active threads" % threading.activeCount())
-            print(state)
+        self._status_leak()
+
+    def _init_leak(self):
+        if not self.leak: return
+        if self.heap: return
+        if guppy:
+            self.heap = guppy.hpy()
+            self.heap.setrelheap()
+
+    def _status_leak(self):
+        if not self.leak: return
+        pid = os.getpid()
+        print("%d active threads" % threading.activeCount())
+        if guppy:
+            print(self.heap.heap())
+        if psutil:
+            process = psutil.Process(pid)
+            print(process.open_files())
+            print(process.memory_info_ex())
+            print(process.memory_maps())
